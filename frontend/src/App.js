740da3535d99c8efea5e2d5d76bfb33649d2d6f8
mokoshin0720 <shinya.work0720@gmail.com>
2022-01-18 02:46:09 +0900

    [move]state to nextSentence

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import QuestionGPT from './bot';
 import './App.css';
-import instance from './axios';
-import requests from './requests';
 
 export const ReplyText = React.createContext();
 
@@ -13,31 +11,10 @@ function App() {
     marginTop: 150
   };
 
-  const [reply, setReply] = useState("JUST A MINUTES...");
-  const dummyText = "It is difficult to study English."
-
-  useEffect(() => {
-    async function fetchData() {
-      const chat_log = {input_text: dummyText}
-      const response = await instance.post(requests.chatGenerate, chat_log);
-      setReply(response.data);
-      return response
-    }
-
-    fetchData();
-  }, []);
-
-  const value = {
-    reply,
-    setReply,
-  }
-
   return (
     <div className="App">
       <div style={style}>
-        <ReplyText.Provider value={value}>
           <QuestionGPT />
-        </ReplyText.Provider>
       </div>
     </div>
   );
--- frontend/src/bot.js
+++ frontend/src/bot.js
@@ -1,6 +1,7 @@
-import React, { useContext } from 'react';
+import React, { useState, useEffect } from "react";
 import ChatBot from 'react-simple-chatbot';
-import { ReplyText } from './App';
+import instance from './axios';
+import requests from './requests';
 
 export const QuestionGPT = () => {
     const dummyText = "It is difficult to study English."
@@ -27,7 +28,19 @@ export const QuestionGPT = () => {
 }
 
 export const NextSentence = () => {
-    const {reply} = useContext(ReplyText);
+    const [reply, setReply] = useState("JUST A MINUTES...");
+    const dummyText = "It is difficult to study English."
+  
+    useEffect(() => {
+      async function fetchData() {
+        const chat_log = {input_text: dummyText}
+        const response = await instance.post(requests.chatGenerate, chat_log);
+        setReply(response.data);
+        return response
+      }
+  
+      fetchData();
+    }, []);
 
     return (
         <div>{reply}</div>
